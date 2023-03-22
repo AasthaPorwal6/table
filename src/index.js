d854ac316b2062e5e35a8dcf3dc883fd7c748821
Aastha <aasthaporwal6@gmail.com>
2023-03-23 00:28:24 +0530

    navigation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import './index.css';
-import App from './App';
 import Home from './Home.js';
 import Table from './Table.js';
-import table from './table.css';
-import home from './home.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home/>
-    <Table/>
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/table" element={<Table />} />
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
